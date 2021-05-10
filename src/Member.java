ba281e3d550f1a4bc7e6349691a6c37b70b02377
Roed <Geofery10@hotmail.com>
2021-05-10 20:29:29 +0200

    Interessent analyse

--- src/Member.java
+++ src/Member.java
@@ -1,3 +1,6 @@
 public abstract class Member {
+  private String name;
+  private String age;
+  private boolean activity;
  // It should be used when various implementations of the same kind share a common behavior.
 }
