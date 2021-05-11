19de4eb21df112120643008e65704ccc2c0a6ed6
Christopher Nielsen <christophernielsen@MacBook-Air-tilhrende-Christopher.local>
2021-05-11 09:56:02 +0200

    Membership class

--- src/Membership.java
+++ src/Membership.java
@@ -1,2 +1,18 @@
 public class Membership {
+
+
+    Member member = new Member();
+
+    public int ageIdentifier() {
+        if (member.age > 18) {
+            //(subscription +1000);
+        } else if (member.age <= 18) {
+            //(subscription + 1600);
+        } else if (member.age < 60) {
+            // (subscription + 1600 * 0.8);
+        } else {
+            //(subscription + 500);
+        }
+        return 0;
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -3,11 +3,11 @@ public class Menu {
 
     System.out.println("\nWelcome to the Dolphins swim club!\n");
     System.out.println("          ");
-    System.out.println("1. ");
-    System.out.println("2. ");
-    System.out.println("3. ");
-    System.out.println("4. ");
-    System.out.println("5. ");
+    System.out.println("1. Join the club");
+    System.out.println("2. View you membership");
+    System.out.println("3. Competition results");
+    System.out.println("4. Lost and found");
+    System.out.println("5. Admin access");
     System.out.println("9. ");
     System.out.println("Please choose: ");
   }
