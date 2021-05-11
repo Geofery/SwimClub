3e43822e5cc1a801d4912c93c042baf9ee3140f3
Christopher Nielsen <christophernielsen@MacBook-Air-tilhrende-Christopher.local>
2021-05-11 11:32:19 +0200

    Membership fix

--- src/Membership.java
+++ src/Membership.java
@@ -1,25 +1,27 @@
 import java.util.ArrayList;
 
 public class Membership {
-    Member member = new Member();
+    Member member;
 
-    Arraylist<member> youthTeam = new ArrayList<>();
-    Arraylist<member> seniorTeam = new ArrayList<>();
-    Arraylist<member> seniorXoTeam = new ArrayList<>();
-    Arraylist<member> passive = new ArrayList<>();
+    ArrayList<Member> youthTeam = new ArrayList<>();
+    ArrayList<Member> seniorTeam = new ArrayList<>();
+    ArrayList<Member> seniorXoTeam = new ArrayList<>();
+    ArrayList<Member> passive = new ArrayList<>();
+
+
 
 
     public int ageIdentifier() {
 
        int getAge = Integer.parseInt(member.getAge());
-        if (getAge > 18) {
-            youthTeam.add.subscription +1000;
-        } else if (getAge <= 18) {
-            seniorTeam.add.seniorTeam.subscription + 1600;
-        } else if (getAge< 60) {
-             seniorXoTeam.add.subscription + 1600 * 0.8;
+        if (getAge < 18) {
+            youthTeam.add(member);
+        } else if (getAge >= 18) {
+            seniorTeam.add(member);
+        } else if (getAge >= 60) {
+             seniorXoTeam.add(member);
         } else {
-             passive.add.subscription + 500;
+             passive.add(member);
         }
         return 0;
     }
